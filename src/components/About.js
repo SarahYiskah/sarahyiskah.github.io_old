ff9470cae8996e8d104789bf87e8c5673f801580
SarahYiskah <sarahy97@gmail.com>
2018-08-03 13:30:55 -0400

    under construction

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ class About extends Component {
   render() {
     return (
       <div id="about" onMouseOver={() => this.props.changeActive("about")}>
-      {/*<div id="alert"><p style={{"fontWeight":"100", "display": "inline-block", "verticalAlign": "middle"}}><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0  24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="8"></line></svg>  Site is under construction</p></div*/}
+      <div id="alert"><p style={{"fontWeight":"100", "display": "inline-block", "verticalAlign": "middle"}}><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0  24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="feather feather-info"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12" y2="8"></line></svg>  Site is under construction</p></div>
       <div id="background"/>
       <div id="aboutContainer" className="info">
         <div className="placeholder" />
