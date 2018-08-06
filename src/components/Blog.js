2c56b03e1f5d9eb53ff558b961d5dee8cb816d65
SarahYiskah <sarahy97@gmail.com>
2018-08-06 00:33:37 -0400

    gave about2 margins, fixed blog post for phone screens, fixed percentages of projects

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -9,9 +9,12 @@ class Blog extends Component {
       <img src={require('../images/blog-background2.png')} />
       <h1 className="title">BLOG</h1>
       <div id="post">
-      <a href="https://medium.com/@97sarahy/good-coding-standards-to-aim-for-477d35501f30" target="_blank"><h2>Good Coding Standards To Aim For</h2>
+      <a href="https://medium.com/@97sarahy/good-coding-standards-to-aim-for-477d35501f30" target="_blank">
+      <div id="smallFlex">
+      <h2>Good Coding Standards To Aim For</h2>
       <p id="deets">Sarah Y. Eichenstein &#8231; Feb 14, 2018 - 4 min read</p>
       <img src={require('../images/coding-pic.jpg')} />
+      </div>
       </a><br/>
       </div>
       <a href="https://medium.com/@97sarahy/" target="_blank"><div id="button">More Articles</div></a>
